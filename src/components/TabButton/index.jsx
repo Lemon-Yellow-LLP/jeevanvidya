9ab64b7ab23e49ded63ec2135bd5b5eebd15c4c7
Anagha-LY <anagha@ly.design>
2023-08-18 18:18:14 +0530

    Implemented tab button

--- src/components/TabButton/index.jsx
+++ src/components/TabButton/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-export default function Navbar() {
-  return <div>Navbar</div>;
+function TabButton({activeTab, label, onChange }) {
+  console.log(activeTab);
+  console.log(label);
+  return (
+    <div
+      className={` ${
+        activeTab === label ? 'bg-[#0084cb28]' : null
+      } w-fit px-6 py-[14px] border-[#0084cb7a] border-[1.5px] rounded-[64px] transition-all duration-300`}
+      onClick={() => onChange(label)}
+    >
+      {label}
+    </div>
+  );
 }
+
+export default TabButton;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import TabButton from '@/components/TabButton';
+import { useState } from 'react';
 
+const types = ['Written Testimonial', 'Video Testimonials', 'test'];
 export default function Home() {
-  return <div>Home</div>;
+  const [active, setActive] = useState(types[0]);
+
+  const handleClick = (e) => {
+    setActive(e);
+    // console.log(e);
+  };
+
+  return (
+    <>
+      {types.map((type, index) => (
+        <TabButton label={type} activeTab={active} onChange={handleClick} key={index}></TabButton>
+      ))}
+    </>
+  );
 }
