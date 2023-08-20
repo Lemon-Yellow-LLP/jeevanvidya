e2400aecc2684bc671d8c2efad8df7c00cc5809a
omkarlydesign <omkar.d@ly.design>
2023-08-20 11:39:18 +0530

    media press page updated

--- src/pages/MediaPress/index.jsx
+++ src/pages/MediaPress/index.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import RegisterArrow from '@/assets/arrowforward.svg';
-import mediaVideo from '@/assets/media_video.svg'
-import Button from '@/components/Button';
 
 export default function MediaPress() {
   const tabs = ['News & Articles', 'Videos', 'Newspapers'];
-  const [activeTab, setActiveTab] = useState('News & Articles');
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+    console.log(tab);
+  };
+
   return (
     <section className='pt-[164px]'>
       <div className='tabsection flex gap-6'>
@@ -16,7 +19,7 @@ export default function MediaPress() {
               className={`py-[14px] whitespace-nowrap md:py-[14px] px-6 rounded-[64px] bg-[#f5f5f5] border-[1.5px] border-[#0084cb29] ${
                 tab === activeTab ? 'active-tab' : ''
               }`}
-              onClick={() => setActiveTab(tab)}
+              onClick={() => handleTabClick(tab)}
             >
               {tab}
             </button>
@@ -24,25 +27,22 @@ export default function MediaPress() {
         })}
       </div>
 
-      <div className='pt-4'>
-        <div class='w-[410px] flex flex-col items-start transform transition duration-500 hover:scale-105 hover:bg-white hover:shadow-lg rounded-xl'>
-          <img src={mediaVideo} class='w-full' alt='' />
-          <div class='p-4 flex flex-col items-start gap-2'>
-            <h3 class='text-[22px] font-semibold text-[#122135]'>Title</h3>
-            <span class='text-base'>
-              Lorem ipsum dolor sit amet, sectre adipiscing elit.Cras molestie blandit...
-            </span>
-            <Button register={true} inputClasses='w-[153px] md:hover:w-[170px]'>
-              Register Now
-              <img
-                src={RegisterArrow}
-                className='ml-2 md:opacity-0 md:group-hover:opacity-100'
-                alt='Register Now'
-              />
-            </Button>
-          </div>
+      {activeTab === tabs[0] && (
+        <div>
+          <div className='text-center'>1</div>
         </div>
-      </div>
+      )}
+      {activeTab === tabs[1] && (
+        <div>
+          <div>2</div>
+        </div>
+      )}
+
+      {activeTab === tabs[2] && (
+        <div>
+          <div>3</div>
+        </div>
+      )}
     </section>
   );
 }
