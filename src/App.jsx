4435c2dbd3889a2a240fd781d22fbde3fd9adf1b
PrathameshShingateLy <prathamesh@ly.design>
2023-08-21 16:18:55 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import Navbar from '@/components/Navbar';
-// import MediaPress from '@/pages/MediaPress';
-import About from './pages/About';
+import MediaPress from '@/pages/MediaPress';
+import About from '@/pages/About';
+import Dnyanpeeth from '@/pages/Dnyanpeeth';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/mediaPress' element={<MediaPress />} />
+        <Route path='/media-press' element={<MediaPress />} />
         <Route path='/about' element={<About />} />
+        <Route path='/dnyanpeeth' element={<Dnyanpeeth />} />
+        <Route path='*' element={<div>Page not found</div>} />
       </Routes>
     </BrowserRouter>
   );
