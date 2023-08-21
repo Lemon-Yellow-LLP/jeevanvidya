671309eedaa4eaa1a5018fd75965c8279ba4d437
omkarlydesign <133740181+omkarlydesign@users.noreply.github.com>
2023-08-21 10:53:01 +0530

    Banner cmp (#2)
    * Add Folder Structure and Routes
    
    * Add Folder Structure and Routes
    
    * Fix: vite config
    
    * button
    
    * updated button components
    
    * navbar
    
    * navbar
    
    * navbar updated
    
    * navbar & hero banner completed
    
    * media and press release component
    
    * banner component created
    
    * Hero Banner updated
    
    ---------
    
    Co-authored-by: rohanly <rohan.k@ly.design>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from '@/pages/Home';
+import Navbar from '@/components/Navbar';
+import MediaPress from '@/pages/MediaPress';
 
 function App() {
   return (
     <BrowserRouter>
+      {/* <Navbar /> */}
       <Routes>
-        <Route path='/' element={<Home />} />
+        {/* <Route path='/' element={<Home />} /> */}
+        <Route path='/' element={<MediaPress />} />
       </Routes>
     </BrowserRouter>
   );
