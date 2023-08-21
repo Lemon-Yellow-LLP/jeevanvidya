0c4f5041e6452069d6dec3bd2871e5b5843f0870
omkarlydesign <133740181+omkarlydesign@users.noreply.github.com>
2023-08-21 10:59:36 +0530

    Navbarstrip (#3)
    * Add Folder Structure and Routes
    
    * Add Folder Structure and Routes
    
    * Fix: vite config
    
    * button
    
    * updated button components
    
    * navbar
    
    * navbar
    
    * navbar updated
    
    * navbar & hero banner completed
    
    ---------
    
    Co-authored-by: rohanly <rohan.k@ly.design>

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import Navbar from '@/components/Navbar';
 import MediaPress from '@/pages/MediaPress';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
-        {/* <Route path='/' element={<Home />} /> */}
-        <Route path='/' element={<MediaPress />} />
+      <Route path='/' element={<MediaPress />} />
       </Routes>
     </BrowserRouter>
   );
