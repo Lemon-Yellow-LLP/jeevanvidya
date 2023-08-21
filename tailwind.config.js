8bc1dcc6bfe26f513e2e5378df6e3612a2e68209
rohanly <rohan.k@ly.design>
2023-08-21 11:38:51 +0530

    Added image-detail-container

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,16 @@ export default {
       modal: '0px 8px 11px -4px #0000000D',
       calendar: '0px 4px 14px 0px #0000001A',
     },
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.1em',
+      widest: '.25em',
+    }
   },
   plugins: [],
 };
