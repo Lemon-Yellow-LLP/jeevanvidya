94aed37b6cf270e17f838509f8311b79575e0402
omkarlydesign <omkar.d@ly.design>
2023-08-22 15:25:28 +0530

    Desktop stepper and mobile stepper

--- src/pages/Courses/index.jsx
+++ src/pages/Courses/index.jsx
@@ -1,27 +1,36 @@
+import DesktopStepper from '@/components/DesktopStepper';
 import Stepper from '@/components/Stepper';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function Courses() {
-  const [currentStep, setCurrentStep] = useState(1);
+  const steps = ['Basic Details', 'Address Details', 'Other Details', 'Success'];
+  const [activeStepIndex, setActiveStepIndex] = useState(0);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
-  const steps = ['Basic Details', 'Address Details', 'Address Details', 'Donation', 'Success'];
+  const goToNextStep = useCallback(() => {
+    setActiveStepIndex((prev) => prev + 1);
+  });
 
-  const handleClick = (valid) => {
-    let newStep = currentStep;
-    valid === 'valid' && newStep++;
-    console.log(newStep);
-    newStep > 0 && newStep <= steps.length - 1 && setCurrentStep(newStep);
-  };
+  useEffect(() => {
+    function handleWindowResize() {
+      setInnerWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
 
   return (
     <div className='md:w-1/2 mx-auto'>
       <div className='container flex flex-col gap-14'>
-        <Stepper steps={steps} currentStep={currentStep} />
-
-        <div>
+        {innerWidth < 768 ? (
+          <Stepper steps={steps} activeStep={activeStepIndex} />
+        ) : (
+          <DesktopStepper steps={steps} activeStep={activeStepIndex} />
+        )}
+        <div className='ml-auto'>
           <button
             className='rounded-lg px-6 py-[9px] md:py-4 text-sm font-normal md:text-base md:font-semibold text-white bg-[#F09444] cursor-pointer'
-            onClick={() => handleClick('valid')}
+            onClick={goToNextStep}
           >
             Save & Next
           </button>
