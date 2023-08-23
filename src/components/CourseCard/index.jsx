6c13e39db00fad57c57c4cdee1bd6d9a0bda6a31
omkarlydesign <omkar.d@ly.design>
2023-08-23 15:51:41 +0530

    course card updated

--- src/components/CourseCard/index.jsx
+++ src/components/CourseCard/index.jsx
@@ -1,44 +1,57 @@
 import Button from '../Button';
 import forwardArrow from '@/assets/learnmore.svg';
+import PropTypes from 'prop-types';
+import { memo } from 'react';
 
-const CourseCard = () => {
-  return (
-    <div className='p-6 w-[302px] bg-[#F5F5F5]'>
-      <div className='flex flex-col gap-4'>
-        <div className='flex flex-col'>
-          <span className='text-base font-normal text-[#0084CB]'>Bicholim, Goa</span>
-          <span className='text-xl font-semibold'>5 Aug 2023 (04:00 PM)</span>
-        </div>
-        <div className='flex gap-4'>
+const CourseCard = memo(
+  ({ courselocation, coursedate, courseseats, contactNumber, donation, address }) => {
+    return (
+      <div className='p-6 w-[302px] bg-[#F5F5F5] md:hover:shadow-2xl rounded-2xl border border-[#0084cb29]'>
+        <div className='flex flex-col gap-4'>
           <div className='flex flex-col'>
-            <span className='text-sm font-normal text-[#122135cc]'>Seat available</span>
-            <span className='text-base font-normal text-[#122135]'>219/250</span>
+            <span className='text-base font-normal text-[#0084CB]'>{courselocation}</span>
+            <span className='text-xl font-semibold'>{coursedate}</span>
+          </div>
+          <div className='flex gap-4'>
+            <div className='flex flex-col'>
+              <span className='text-sm font-normal text-[#122135cc]'>Seat available</span>
+              <span className='text-base font-normal text-[#122135]'>{courseseats}/250</span>
+            </div>
+            <div className='flex flex-col'>
+              <span className='text-sm font-normal text-[#122135cc]'>Contact Number</span>
+              <span className='text-base font-normal text-[#122135]'>{contactNumber}</span>
+            </div>
           </div>
           <div className='flex flex-col'>
-            <span className='text-sm font-normal text-[#122135cc]'>Contact Number</span>
-            <span className='text-base font-normal text-[#122135]'>9922497333</span>
+            <span className='text-sm font-normal text-[#122135cc]'>Donation</span>
+            <span className='text-base text-[#122135]'>₹ {donation}</span>
           </div>
-        </div>
-        <div className='flex flex-col'>
-          <span className='text-sm font-normal text-[#122135cc]'>Donation</span>
-          <span className='text-base text-[#122135]'>₹ 1500</span>
-        </div>
-        <div className='flex flex-col'>
-          <span className='text-sm font-normal text-[#122135cc]'>Address</span>
-          <span className='text-base text-[#122135]'>Shetye Hall, Sonar Peth, Bicholim, Goa</span>
-        </div>
+          <div className='flex flex-col'>
+            <span className='text-sm font-normal text-[#122135cc]'>Address</span>
+            <span className='text-base text-[#122135]'>{address}</span>
+          </div>
 
-        <Button variant='outline' className='group w-[254px] justify-center'>
-          Register Now
-          <img
-            src={forwardArrow}
-            alt='forwardArrow'
-            className='ml-2 md:opacity-0 md:group-hover:opacity-100 md:group-hover:pl-3 transform transition-all duration-300'
-          />
-        </Button>
+          <Button variant='outline' className='group w-[254px] justify-center'>
+            Register Now
+            <img
+              src={forwardArrow}
+              alt='forwardArrow'
+              className='ml-2 md:opacity-0 md:group-hover:opacity-100 md:group-hover:pl-3 transform transition-all duration-300'
+            />
+          </Button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
 
 export default CourseCard;
+
+CourseCard.propTypes = {
+  courselocation: PropTypes.string,
+  coursedate: PropTypes.string,
+  courseseats: PropTypes.number,
+  contactNumber: PropTypes.number,
+  donation: PropTypes.number,
+  address: PropTypes.string,
+};
