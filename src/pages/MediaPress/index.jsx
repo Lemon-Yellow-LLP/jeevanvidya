620ae1e1538b73d3a3468e0c2511fa9cbb1d4aeb
omkarlydesign <omkar.d@ly.design>
2023-08-24 13:52:57 +0530

    Media press page updated

--- src/pages/MediaPress/index.jsx
+++ src/pages/MediaPress/index.jsx
@@ -1,8 +1,91 @@
+import HeroBanner from '@/components/HeroBanner';
+import TabButton from '@/components/TabButton';
+import MediaCard from '@/components/MediaCard';
 import { useState } from 'react';
+import mediaBanner from '@/assets/mediapressBanner.png';
+import newsarticlesImg from '@/assets/newsarticlesImg.png';
+import VideoImg from '@/assets/videoImg.png';
+import NewsImg from '@/assets/newspaper1.png';
+
+const newsData = [
+  {
+    image: newsarticlesImg,
+    slug: 'News & Articles',
+    title: 'Title',
+    desc: 'Lorem ipsum dolor sit amet, sectetur adipiscing elit. Cras molestie blandit...',
+  },
+  {
+    image: newsarticlesImg,
+    slug: 'News & Articles',
+    title: 'Title',
+    desc: 'Lorem ipsum dolor sit amet, sectetur adipiscing elit. Cras molestie blandit...',
+  },
+  {
+    image: newsarticlesImg,
+    slug: 'News & Articles',
+    title: 'Title',
+    desc: 'Lorem ipsum dolor sit amet, sectetur adipiscing elit. Cras molestie blandit...',
+  },
+  {
+    image: newsarticlesImg,
+    slug: 'News & Articles',
+    title: 'Title',
+    desc: 'Lorem ipsum dolor sit amet, sectetur adipiscing elit. Cras molestie blandit...',
+  },
+];
+
+const VideoData = [
+  {
+    image: VideoImg,
+    slug: 'Videos',
+    title: 'Video Title 1',
+    desc: 'Lorem ipsum dolor sit amet, sectetur adipiscing elit. Cras molestie blandit...',
+  },
+  {
+    image: VideoImg,
+    slug: 'Videos',
+    title: 'Video Title 1',
+    desc: 'Lorem ipsum dolor sit amet, sectetur adipiscing elit. Cras molestie blandit...',
+  },
+  {
+    image: VideoImg,
+    slug: 'Videos',
+    title: 'Video Title 1',
+    desc: 'Lorem ipsum dolor sit amet, sectetur adipiscing elit. Cras molestie blandit...',
+  },
+];
+
+const NewspaperData = [
+  {
+    image: NewsImg,
+    slug: 'Newspapers',
+  },
+  {
+    image: NewsImg,
+    slug: 'Newspapers',
+  },
+  {
+    image: NewsImg,
+    slug: 'Newspapers',
+  },
+  {
+    image: NewsImg,
+    slug: 'Newspapers',
+  },
+  {
+    image: NewsImg,
+    slug: 'Newspapers',
+  },
+  {
+    image: NewsImg,
+    slug: 'Newspapers',
+  },
+];
 
 export default function MediaPress() {
   const tabs = ['News & Articles', 'Videos', 'Newspapers'];
   const [activeTab, setActiveTab] = useState(tabs[0]);
+  // const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
@@ -10,39 +93,51 @@ export default function MediaPress() {
   };
 
   return (
-    <section className='pt-[164px]'>
-      <div className='tabsection flex gap-6'>
-        {tabs.map((tab, i) => {
-          return (
-            <button
-              key={i}
-              className={`py-[14px] whitespace-nowrap md:py-[14px] px-6 rounded-[64px] bg-[#f5f5f5] border-[1.5px] border-[#0084cb29] ${
-                tab === activeTab ? 'active-tab' : ''
-              }`}
-              onClick={() => handleTabClick(tab)}
-            >
-              {tab}
-            </button>
-          );
-        })}
+    <section className=''>
+      <HeroBanner
+        bannerImg={mediaBanner}
+        bannerTitle='Media and Press Releases'
+        bannerDesc='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '
+      />
+
+      {/* Tab section */}
+      <div className='p-3'>
+        <div className='flex gap-2'>
+          {tabs.map((tab, index) => (
+            <TabButton label={tab} activeTab={activeTab} onChange={handleTabClick} key={index} />
+          ))}
+        </div>
+
+        <div className='md:p-20 grid grid-cols-3'>
+          {activeTab === tabs[0] &&
+            newsData.map((data, i) => (
+              <MediaCard
+                key={i}
+                cardTitle={data.title}
+                cardDesc={data.desc}
+                cardImage={data.image}
+                btnTitle='Read More'
+              />
+            ))}
+
+          {activeTab === tabs[1] &&
+            VideoData.map((data, i) => (
+              <MediaCard
+                key={i}
+                cardTitle={data.title}
+                cardDesc={data.desc}
+                cardImage={data.image}
+              />
+            ))}
+
+          {activeTab === tabs[2] &&
+            NewspaperData.map((data, i) => (
+              <div key={i}>
+                <img src={data.image} alt={data.slug} className='' />
+              </div>
+            ))}
+        </div>
       </div>
-
-      {activeTab === tabs[0] && (
-        <div>
-          <div className='text-center'>1</div>
-        </div>
-      )}
-      {activeTab === tabs[1] && (
-        <div>
-          <div>2</div>
-        </div>
-      )}
-
-      {activeTab === tabs[2] && (
-        <div>
-          <div>3</div>
-        </div>
-      )}
     </section>
   );
 }
