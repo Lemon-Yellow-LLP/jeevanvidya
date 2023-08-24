6acd35172de80b3b61b94f65e08b6e71aa609e9b
PrathameshShingateLy <Prathamesh@ly>
2023-08-24 18:36:06 +0530

    added course card component

--- src/components/CourseCard/index.jsx
+++ src/components/CourseCard/index.jsx
@@ -3,13 +3,19 @@ import Button from '../Button';
 
 const CourseCard = ({ image, title, desc, duration, ageGroup }) => {
   return (
-    <div className='w-full p-4'>
-      <img src={image} alt='' />
-      <h2>{title}</h2>
-      <p>{desc}</p>
-      <p>Duration: {duration}</p>
-      <p>Age Group: {ageGroup}</p>
-      <Button register={true} variant='outline' className='w-[153px] md:hover:w-[170px]'>
+    <div className='w-full p-4 rounded-xl border-[1px] border-dark-white'>
+      <img src={image} alt='' className='rounded-xl h-[152px]' />
+      <h2 className='text-primary-black font-semibold mt-4'>{title}</h2>
+      <p className='text-primary-black text-sm font-normal mt-3 opacity-80'>{desc}</p>
+      <p className='text-primary-black flex gap-10 mt-2'>
+        <span className='opacity-80'>Duration:</span>
+        <span className='font-semibold'>{duration}</span>
+      </p>
+      <p className='text-primary-black flex gap-6 mt-2'>
+        <span className='opacity-80'>Age Group:</span>
+        <span className='font-semibold'>{ageGroup}</span>
+      </p>
+      <Button register={true} variant='outline' className='flex justify-center mt-4'>
         Register Now
       </Button>
     </div>
