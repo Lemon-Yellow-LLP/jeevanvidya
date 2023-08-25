b0b0641a66491301b09dfe3b7288a365c6195b29
omkarlydesign <omkar.d@ly.design>
2023-08-25 12:10:04 +0530

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,12 +19,17 @@ export default {
           1: '#122135',
           2: '#122135CC',
         },
+        background: {
+          1: 'linear-gradient(180deg, #E2EAF4 0%, rgba(226, 234, 244, 0.00) 100%)',
+          2: 'linear-gradient(135deg, #0352A3 0%, #0084CB 100%, #0084CB 100%)',
+          lightblue: '#E2EAF4',
+        },
         dialog: {
           1: '#ffffffcc',
         },
       },
       boxShadow: {
-        primary: '',
+        primary: '0px 3px 48px 0px #0353a33d',
         secondary: '0px 4px 44px 0px #3267e314',
       },
     },
