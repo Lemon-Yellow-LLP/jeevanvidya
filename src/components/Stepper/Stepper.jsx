d28f0d0eeaf36d9346e3b5f82e16303b2c224ac2
rohanly <141114696+rohanly@users.noreply.github.com>
2023-09-04 15:39:47 +0530

    Stepper Updated (#51)
    Co-authored-by: omkarlydesign <omkar.d@ly.design>

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import MobileStepper from './mobile';
-import DesktopStepper from './desktop';
+import MobileStepper from '.';
+import DesktopStepper from '../DesktopStepper';
 
 const Stepper = ({ className, buttonTitle, steps }) => {
   const [activeStepIndex, setActiveStepIndex] = useState(0);
@@ -20,24 +20,22 @@ const Stepper = ({ className, buttonTitle, steps }) => {
     return () => window.removeEventListener('resize', handleWindowResize);
   }, []);
   return (
-    <div className={`w-full mx-auto mt-4 flex flex-col  ${className}`}>
+    <div className={`w-full mx-auto mt-4 flex flex-col gap-14 ${className}`}>
       {innerWidth < 768 ? (
         <MobileStepper steps={steps} activeStep={activeStepIndex} progress={progress} />
       ) : (
         <DesktopStepper steps={steps} activeStep={activeStepIndex} />
       )}
-      {buttonTitle ? (
-        <div className='ml-auto'>
-          <button
-            className='rounded-lg px-6 py-[9px] md:py-4 text-sm font-normal md:text-base md:font-semibold text-white bg-[#F09444] cursor-pointer'
-            onClick={goToNextStep}
-          >
-            {buttonTitle}
-          </button>
-        </div>
-      ) : null}
+      <div className='ml-auto'>
+        <button
+          className='rounded-lg px-6 py-[9px] md:py-4 text-sm font-normal md:text-base md:font-semibold text-white bg-[#F09444] cursor-pointer'
+          onClick={goToNextStep}
+        >
+          {buttonTitle}
+        </button>
+      </div>
     </div>
   );
 };
 
-export default MobileStepper;
+export default Stepper;
