2e8d1c2b0cfbfecca51a633a77f0cb989214262e
omkarlydesign <omkar.d@ly.design>
2023-09-05 17:50:37 +0530

    Testimonial Page Updated

--- src/pages/Testimonial/index.jsx
+++ src/pages/Testimonial/index.jsx
@@ -3,7 +3,7 @@ import TabButton from '@/components/TabButton';
 import SectionTitleDescription from '@/components/SectionTitleDescription';
 import TestimonialCard from '@/components/TestimonialCard';
 import HeroBanner from '@/components/HeroBanner';
-import Flower from '@/assets/section-flower.png';
+import Flower from '@/assets/testimonial-flower.svg';
 import testImage from '@/assets/Testimonialimg.png';
 import videoImage from '@/assets/video.png';
 import vectorImg from '@/assets/testimonial_pattern.png';
@@ -317,9 +317,9 @@ const Testimonial = () => {
         <img
           src={vectorImg}
           alt='vector page'
-          className='absolute right-0 top-[0%] opacity-5 w-[332px] h-[318px] xl:w-[587px] xl:h-[732px]'
+          className='absolute right-0 top-[0%] opacity-10 w-[204px] h-[251px] xl:w-[587px] xl:h-[732px]'
         />
-        <div className='flex justify-between items-center max-md:flex-col'>
+        <div className='flex justify-between items-center max-md:flex-col z-10'>
           <SectionTitleDescription
             className='mx-0'
             align='start'
@@ -377,7 +377,7 @@ const Testimonial = () => {
           Want to Submit Testimonial?
         </span>
 
-        <div className='bg-[white] rounded-[64px] flex p-[4px] border-[1.5px] border-[#0084CB29] gap-[5px]'>
+        <div className='bg-[white] rounded-[64px] flex p-[4px] border-[1.5px] border-[#0084CB29] gap-[5px] w-full'>
           {tabOptions?.map((e, index) => {
             return activeTab === e.label ? (
               <TabButton
@@ -385,12 +385,13 @@ const Testimonial = () => {
                 label={e.label}
                 activeTab={activeTab}
                 onChange={handleTabChange}
+                className="py-[14px] px-3 text-base"
               />
             ) : (
               <span
                 key={index}
                 onClick={() => handleTabChange(e.label)}
-                className='w-fit px-6 py-[14px] transition-all duration-300 cursor-pointer max-md:text-center'
+                className='w-fit text-center text-base py-[14px] px-3'
               >
                 {e.label}
               </span>
